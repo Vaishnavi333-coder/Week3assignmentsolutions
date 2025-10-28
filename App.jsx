880ee648fd3a131642218bcba95cf0d98cd4cbcc
Vaishnavi333-coder <151718327+Vaishnavi333-coder@users.noreply.github.com>
2025-10-28 16:17:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import ProductCatalog from './components/ProductCatalog'
+
+function App() {
+  // Static, presentation-only app: render the ProductCatalog component
+  return <ProductCatalog />
+}
+
+export default App
